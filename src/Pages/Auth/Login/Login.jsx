07fb5ab4cf5b8efebcb9f763f2f82f2f3aa73ba3
unknown <abdullahalrahman23@gmail.com>
2024-11-07 11:07:40 +0300

    add:authentication

--- src/Pages/Auth/Login/Login.jsx
+++ src/Pages/Auth/Login/Login.jsx
@@ -1,7 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import event1 from "../../../assets/Images/Work anniversary-bro.png";
+import { ToastContainer, toast } from "react-toastify";
+import { AuthContext } from "../../../AuthProvider/AuthProvider";
 const Login = () => {
+  const { signInUser, sendEmailVerification } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    signInUser(email, password)
+      .then((res) => {
+        const user = res.user;
+        if (res.user?.emailVerified) {
+          e.target.reset();
+          navigate("/");
+        } else {
+          sendEmailVerification(user).then(() => {
+            toast("Verification email sent");
+          });
+
+          setTimeout(() => {
+            e.target.reset();
+            navigate("/home");
+          }, 4000);
+        }
+      })
+      .catch((err) => {
+        toast("Password is incorrect");
+        // console.log(err.message);
+      });
+  };
+
   return (
     <div className="hero  min-h-screen  ">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -9,13 +42,14 @@ const Login = () => {
           <img src={event1} />
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form className="card-body">
+          <form onSubmit={handleSignIn} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
                 type="email"
+                name="email"
                 placeholder="email"
                 className="input input-bordered"
                 required
@@ -27,6 +61,7 @@ const Login = () => {
               </label>
               <input
                 type="password"
+                name="password"
                 placeholder="password"
                 className="input input-bordered"
                 required
@@ -38,10 +73,9 @@ const Login = () => {
               </label>
             </div>
             <div className="form-control mt-6">
-              <Link to={"/home"} className="btn bg-[#447af4] text-white">
-                Login
-              </Link>
+              <button className="btn bg-[#447af4] text-white">Login</button>
             </div>
+            <ToastContainer></ToastContainer>
           </form>
           <div className="px-10 py-4">
             <Link to={"/register"} className="text-[#447af4]">
