f86ee09635f17be5c4a18707a8dd19ec80c60ed7
unknown <abdullahalrahman23@gmail.com>
2024-11-14 21:13:30 +0300

    set:time format from time string

--- src/Components/RecentCreatedEvent/EachEvent.jsx
+++ src/Components/RecentCreatedEvent/EachEvent.jsx
@@ -3,6 +3,22 @@ import React from "react";
 const EachEvent = ({ eachevent }) => {
   const { email, name, starttime, endtime, location, eventType, imageLink } =
     eachevent;
+
+  const startDate = new Date(starttime);
+  const endDate = new Date(endtime);
+
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  };
+
+  const formatedStartDate = startDate.toLocaleString("en-US", options);
+  const formatedEndDate = endDate.toLocaleString("en-US", options);
+
   return (
     <div className="card card-side bg-base-100 shadow-xl mt-10">
       <figure>
@@ -10,7 +26,10 @@ const EachEvent = ({ eachevent }) => {
       </figure>
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
-        <p>Start Time:{starttime}</p>
+        <div>
+          <p>Start Time: {formatedStartDate}</p>
+          <p>End time: {formatedEndDate}</p>
+        </div>
       </div>
     </div>
   );
