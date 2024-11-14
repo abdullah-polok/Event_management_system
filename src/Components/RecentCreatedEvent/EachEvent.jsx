7e2e5dd46c3a5e8d04093eb9578d04e2055db30f
unknown <abdullahalrahman23@gmail.com>
2024-11-14 21:22:47 +0300

    fix:text formating of each event

--- src/Components/RecentCreatedEvent/EachEvent.jsx
+++ src/Components/RecentCreatedEvent/EachEvent.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-
+import { FaMapLocationDot } from "react-icons/fa6";
 const EachEvent = ({ eachevent }) => {
   const { email, name, starttime, endtime, location, eventType, imageLink } =
     eachevent;
 
+  ///call date object
   const startDate = new Date(starttime);
   const endDate = new Date(endtime);
 
+  ///creat time format template
   const options = {
     year: "numeric",
     month: "long",
@@ -16,6 +18,7 @@ const EachEvent = ({ eachevent }) => {
     hour12: true,
   };
 
+  ///format the time
   const formatedStartDate = startDate.toLocaleString("en-US", options);
   const formatedEndDate = endDate.toLocaleString("en-US", options);
 
@@ -25,10 +28,14 @@ const EachEvent = ({ eachevent }) => {
         <img className="w-56 h-64" src={imageLink} alt="Movie" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">{name}</h2>
-        <div>
+        <h2 className="card-title text-[#447af4] font-bold">{name}</h2>
+        <h2 className="text-base">Event type: {eventType}</h2>
+        <div className="mt-4 text-sm">
           <p>Start Time: {formatedStartDate}</p>
           <p>End time: {formatedEndDate}</p>
+        </div>
+        <div className="flex items-center gap-2 mt-6 text-sm">
+          <FaMapLocationDot /> {location}
         </div>
       </div>
     </div>
